3da6ce052e48b7f1be0e7cee247cb3219af84754
Meklit Cherie <makywonde347@yahoo.com>
2020-09-23 19:11:03 -0700

    I added some responses and changed the user response for the second line

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 
 # this is a new python file
+from random import choice
 
-# Create a function called get_bot_response(). This function must: 
 
-# It should have 1 parameter called user_response, which is a string with the users input.
-    
-    def get_therapy_bot_response(user_response):
+# Create a function called get_bot_response(). 
+# It should have 1 parameter called user_response
+def get_therapy_bot_response(user_response):
 
-#   It should use at least 2 lists to store at least 3 unique responses to different user inputs.
 
-  bot_response_lonely = ["sorry, you had to go through that, how can I help!", "Keep smiling!life will get better ", "I love to see you happy! what can I do?"]
-  bot_response_tired = ["Go to the park", "meditate", "ouch, go drink a coffee"]
-  bot_response_depressed = ["Go to the gym", "call your friends", "aww, you want to talk about it?"]
+  
+  bot_response_depressed = ["how can I help yoou?", "go take a walk at the park", "I want to see you happy!come home"]
+  bot_response_excited = ["yes whats the news", "yes positive energy only"]
+  bot_response_lazy= ["get up go to the gym","whatever it is, don't give up!"]
+# It should return a string with the chat bot’s response. 
+  if user_response == "depressed":
+    return choice(bot_response_depressed)
+  elif user_response == "excited":
+    return choice(bot_response_excited)
+  elif user_response == "lazy":
+    return choice(bot_response_lazy)
+  else:
+    return "I hope your day gets better"
 
-# Use conditionals to decide which of the response lists to select from.
-# Use choice() to randomly select one of the three responses. 
 
-  if user_response == "lonely":
-      return choice(bot_response_lonely)
-  elif user_response == "tired":
-    return choice(bot_response_tired)
-    if user_response == "depressed":
-      return choice(bot_response_depressed)
+print("Greetings to Therapy Bot")
+print("Please enter how you are feeling this evening?")
 
+
+user_response = ""
+
+while True:
+  user_response = input("How are you feeling today?: ")
+  
+
+  if user_response == 'done':
+    break
+
+  
+  bot_response = get_therapy_bot_response(user_response)
+  print(bot_response)
 
 
 
